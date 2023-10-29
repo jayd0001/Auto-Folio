37df5c32fc6583ebe910d4c3ce65817e6cf64f9e
Jaideep Singh <jay61226@gmail.com>
2023-10-30 01:36:29 +0530

    Car catalogue section- searchFilter, Searchbar, SearchManufacturer compnents implentation along with headless UI installation

---
+++ components/SearchBar.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { useState } from "react"
+import { SearchManufacturer } from "."
+
+const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("")
+  const handleSearch = () => {}
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__items">
+        <SearchManufacturer manuFacturer={manufacturer} setManuFacturer={setManufacturer} />
+      </div>
+    </form>
+  )
+}
+
+export default SearchBar
